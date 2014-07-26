caaea52b858b85e28c50d99fe8a2247e4911e717
xupingmao <xupingmao@gmail.com>
2014-07-26 17:06:46 +0800

    Update gc.c

--- gc.c
+++ gc.c
@@ -25,13 +25,7 @@ void gc_free( void* ptr)
 
 gc_root* gc_init()
 {
-  __gc_root = gc_malloc( sizeof( gc_root) );
-  gc_obj* obj = gc_obj_new(NULL);
-  gc_ref* ref = gc_ref_new(obj, obj);
-  __gc_root->obj_head = obj;
-  __gc_root->obj_tail = obj;
-  __gc_root->ref_head = ref;
-  __gc_root->ref_tail = ref;
+  __gc_root = gc_obj_new("root");
   return __gc_root;
 }
 
@@ -40,22 +34,26 @@ gc_obj* gc_obj_new(void* value)
   gc_obj* obj = gc_malloc( sizeof( gc_obj ) );
   obj->marked = 1;
   obj->value = value;
+  obj->ref_head = NULL;
+  obj->ref_tail = NULL;
   return gc_obj;
 }
 
 void gc_ref_new(gc_obj* father, gc_obj* son)
 {
-  gc_ref* ref = gc_malloc( sizeof( gc_ref ) );
-  ref->father = father;
-  ref->son = son;
-  ref->next = NULL;
-  __gc_root->ref_tail->next = ref;
-  __gc_root->ref_tail = ref;
+  gc_ref* tail = father->ref_tail;
+  if( tail == NULL ){
+    father->ref_tail = son;
+    father->ref_head = son;
+  }else{
+    tail->next = son;
+    father->ref_tail = son;
+  }
 }
 
 gc_obj* gc_global_obj_new(void* value) 
 {
   gc_obj* obj = gc_obj_new(value);
-  gc_ref_new(__gc_root->obj_root, obj);
+  gc_ref_new(__gc_root, obj);
   return obj;
 }
