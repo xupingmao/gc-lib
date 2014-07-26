92cb103f4b3de551387c38b08c595ba49e5bb8f6
xupingmao <xupingmao@gmail.com>
2014-07-26 17:02:31 +0800

    Update gc.h

--- gc.h
+++ gc.h
@@ -10,29 +10,31 @@ typedef struct gc_obj
 {
   int marked;
   void* value;
+  struct gc_ref* ref_head;
+  struct gc_ref* ref_tail;
 }gc_obj;
 
 typedef struct gc_ref
 {
   gc_obj* parent;
-  gc_obj* child;
+  gc_ref* next;
 }gc_ref;
   
+/*
 typedef struct gc_root
 {
-  int pool_size;
-  int obj_used;
-  int ref_used;
-  int obj_total;
-  int ref_total;
-  gc_obj* obj_pool;
-  gc_ref* ref_pool;
+  int obj_used; // 已使用的对象数量
+  int ref_used; // 已使用的引用数量
+  int obj_total; // 全部对象的数量
+  int ref_total; // 全部引用的数量
+  gc_obj* obj_pool; // 存放对象的内存池
+  gc_ref* ref_pool; // 存放引用的内存池
 }gc_root;
+*/
 
+gc_obj* __gc_root;
 
-gc_root* __gc_root;
-
-gc_root* gc_init();
+gc_obj* gc_init();
 void gc_destroy();
 void gc_error(char*msg);
 void* gc_malloc(size_t size);
